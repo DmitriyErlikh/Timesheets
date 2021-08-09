63632a227956faa714bd6a0f20f9a9c58b4b14a0
dmitriyerlikh@gmail.com <dmitriyerlikh@gmail.com>
2021-08-09 23:10:33 +0400

    HomeWork#5

---
+++ Timesheets/Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timesheets.Models.Entities;
+
+namespace Timesheets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ClientController : ControllerBase
+    {
+        public List<Client> Clients = new List<Client>();
+
+        [HttpGet("readclientlist")]
+        public IActionResult GetClientList()
+        {
+            foreach(var client in Clients)
+            {
+                Console.WriteLine(client);
+            }
+            return Ok();
+        }      
+
+        [HttpPut("createclient")]
+        public IActionResult CreateClient([FromQuery] Client client)
+        {
+            Clients.Add(client);
+            return Ok(0);
+        }
+
+        [HttpDelete("deleteclient")]
+        public IActionResult DeleteClient([FromQuery] Client client)
+        {
+            Clients.Remove(client);
+            return Ok(0);
+        }
+    }
+}
---
+++ Timesheets/Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetItem(Guid id);
+        Task<IEnumerable<T>> GetItems();
+        Task Add(T item);
+        Task Delite(T item);
+    }
+}
