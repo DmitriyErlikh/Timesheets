05d7749c35e2159be504c20b0cdd34c707515e99
dmitriyerlikh@gmail.com <dmitriyerlikh@gmail.com>
2021-08-09 23:10:33 +0400

    Добавьте файлы проекта.

---
+++ Timesheets/Models/Entities/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Models.Entities
+{
+    public class Invoice
+    {
+        public Guid Id { get; protected set; }
+        public Guid ContractId { get; protected set; }
+        public DateTime DateStart { get; protected set; }
+        public DateTime DateEnd { get; protected set; }
+        public decimal Sum { get; protected set; }
+
+        public Contract Contract { get; protected set; }
+    }
+}
