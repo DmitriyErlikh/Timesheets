aff1bbe89a1383d36995157dc5c074f417767040
dmitriyerlikh@gmail.com <dmitriyerlikh@gmail.com>
2021-08-09 23:10:33 +0400

    HomeWork#3

---
+++ Timesheets/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timesheets.Models.Entities;
+
+namespace Timesheets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public List<Employee> Employees = new List<Employee>();
+
+        [HttpGet("reademployeelist")]
+        public IActionResult GetEmployeeList()
+        {
+            foreach (var employee in Employees)
+            {
+                Console.WriteLine(employee);
+            }
+            return Ok();
+        }
+
+        [HttpPut("createemployee")]
+        public IActionResult CreateEmployee([FromQuery] Employee employee)
+        {
+            Employees.Add(employee);
+            return Ok(0);
+        }
+
+        [HttpDelete("deleteemployee")]
+        public IActionResult DeleteEmployee([FromQuery] Employee employee)
+        {
+            Employees.Remove(employee);
+            return Ok(0);
+        }
+    }
+}
---
+++ Timesheets/Models/Entities/Contract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Models.Entities
+{
+    public class Contract
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Description { get; set; }
+
+        public List<Contract> Contracts = new List<Contract>();
+    }
+}
